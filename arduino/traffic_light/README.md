d4f9ca273f8bf6520d8f95bbcf3848af7c74d2d1
Aleksandr Martiushov <amartyushov@gmail.com>
2019-06-16 09:58:11 +0200

    arduino: traffic light

---
+++ arduino/traffic_light/README.md
@@ -0,0 +1,7 @@
+This project implements simple traffic light model.  
+
+Notes. R1=R2=R3=220.
+
+![How to assemble the parts](maket_schema.jpg)
+
+![The principal electronic schema](principal_schema.jpg)
---
+++ arduino/traffic_light/traffic_light.ino
@@ -0,0 +1,16 @@
+
+int led_red = 4;
+int led_yellow = 3;
+int led_green = 2;
+
+void setup() {
+  pinMode(led_red, OUTPUT);
+  pinMode(led_yellow, OUTPUT);
+  pinMode(led_green, OUTPUT);
+
+}
+
+void loop() {
+
+ 
+}
