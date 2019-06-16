6b1d791b49c2c9881b5462fc899ffeb59711877f
Aleksandr Martiushov <amartyushov@gmail.com>
2019-06-16 10:12:38 +0200

    source code is updated

--- arduino/traffic_light/traffic_light.ino
+++ arduino/traffic_light/traffic_light.ino
@@ -1,4 +1,3 @@
-
 int led_red = 4;
 int led_yellow = 3;
 int led_green = 2;
@@ -7,10 +6,21 @@ void setup() {
   pinMode(led_red, OUTPUT);
   pinMode(led_yellow, OUTPUT);
   pinMode(led_green, OUTPUT);
-
 }
 
 void loop() {
-
- 
+  digitalWrite(led_red, HIGH);
+    delay(1000);
+  digitalWrite(led_yellow, HIGH);
+    delay(200);
+  
+  digitalWrite(led_green, HIGH);
+  digitalWrite(led_red, LOW);
+  digitalWrite(led_yellow, LOW);
+    delay(1000);
+  
+  digitalWrite(led_yellow, HIGH);
+  digitalWrite(led_green, LOW);
+    delay(200);
+  digitalWrite(led_yellow, LOW); 
 }
